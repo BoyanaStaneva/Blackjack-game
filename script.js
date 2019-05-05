dfd6717db22a4f0360dc0085384ae2b97c742ef4
Boyana Staneva <boyana@rudor.btn1.brandwatch.net>
2019-05-05 17:29:37 +0100

    Starting work on Blackjack game. No functionality yet.

---
+++ script.js
@@ -0,0 +1,7 @@
+let cardOne = "Ace of Spades"
+
+let cardTwo = "Three of Clubs"
+
+document.getElementById('playerHand').innerHTML = `<p>Player drew ${cardOne}</p>`;
+
+document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${cardTwo}</p>`;
