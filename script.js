70dac6882fbf1a0bb2cd6cccfb1913e0a027ff38
Boyana Staneva <boyana@rudor.btn1.brandwatch.net>
2019-05-06 21:55:30 +0100

    The game now has a function that generates a random number

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-let cardOne = "Ace of Spades"
-
-let cardTwo = "Three of Clubs"
-
+// creating array with all the suits
 let suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
+//creating array with all the cards
+let cards = ["Ace", "King", "Queen", "Jack","Two", "Three","Four","Five", "Six","Seven", "Eight", "Nine", "Ten"]
+//creating empty array for the card values and create a deck
 
-let cards = ["Ace", "King", "Queen", "Jack",
-
-  "Two", "Three","Four","Five", "Six","Seven", "Eight", "Nine", "Ten"]
-
-let myDeck = [];
-
+function createDeck () {
+  let myDeck = [];
+//looping through each item from the suits
   for (suitsIndx = 0; suitsIndx < suits.length; suitsIndx++) {
+    //looping through each item from the card values
     for (cardsIndx = 0; cardsIndx < cards.length; cardsIndx++) {
+      //pushing the combinations into a the deck array
       myDeck.push(cards[cardsIndx] + " of " + suits[suitsIndx])
     }
   }
+  return myDeck
+}
 
-  for (let i=0; i < myDeck.length; i++) {
-    console.log(myDeck[i])
-  }
+let myDeck = createDeck();
 
-document.getElementById('playerHand').innerHTML = `<p>Player drew ${myDeck[4]}</p>`;
+function getCard() {
+  return Math.floor((Math.random() * 51) + 1);
+}
 
-document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${myDeck[23]}</p>`;
+
+//adjusting inner html to display the card for the player
+document.getElementById('playerHand').innerHTML = `<p>Player drew ${myDeck[getCard()]}</p>`;
+
+document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${myDeck[getCard()]}</p>`;
