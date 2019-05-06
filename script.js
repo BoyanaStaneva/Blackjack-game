cfbb6a7c91de34fc2c5e2cba7ef54faa4a992b4f
Boyana Staneva <boyana@rudor.btn1.brandwatch.net>
2019-05-06 15:59:26 +0100

    Generated a deck of cards using Arrays and for loops

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let cardOne = "Ace of Spades"
 
 let cardTwo = "Three of Clubs"
 
-document.getElementById('playerHand').innerHTML = `<p>Player drew ${cardOne}</p>`;
+let suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
 
-document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${cardTwo}</p>`;
+let cards = ["Ace", "King", "Queen", "Jack",
+
+  "Two", "Three","Four","Five", "Six","Seven", "Eight", "Nine", "Ten"]
+
+let myDeck = [];
+
+  for (suitsIndx = 0; suitsIndx < suits.length; suitsIndx++) {
+    for (cardsIndx = 0; cardsIndx < cards.length; cardsIndx++) {
+      myDeck.push(cards[cardsIndx] + " of " + suits[suitsIndx])
+    }
+  }
+
+  for (let i=0; i < myDeck.length; i++) {
+    console.log(myDeck[i])
+  }
+
+document.getElementById('playerHand').innerHTML = `<p>Player drew ${myDeck[4]}</p>`;
+
+document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${myDeck[23]}</p>`;
