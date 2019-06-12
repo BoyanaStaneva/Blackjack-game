473c367232679c5f2461372979b92d8c205ea821
Boyana Staneva <boyana@rudor.btn1.brandwatch.net>
2019-06-12 13:41:45 +0100

    The deck is now created as an array of obobjects that hold the value and suit for each card

--- script.js
+++ script.js
@@ -11,20 +11,31 @@ function createDeck () {
     //looping through each item from the card values
     for (cardsIndx = 0; cardsIndx < cards.length; cardsIndx++) {
       //pushing the combinations into a the deck array
-      myDeck.push(cards[cardsIndx] + " of " + suits[suitsIndx])
+      let card = {
+        suit: suits[suitsIndx],
+        value: cards[cardsIndx]
+      }
+      myDeck.push(card)
     }
   }
   return myDeck
 }
 
-let myDeck = createDeck();
+let makeDeck = createDeck();
+
+function makeCardName(i) {
+  return makeDeck[i].value + ' of ' + makeDeck[i].suit
+  console.log(makeDeck[i].value + ' of ' + makeDeck[i].suit);
+}
 
 function getCard() {
   return Math.floor((Math.random() * 51) + 1);
 }
 
+let playerDraw = makeCardName(getCard())
+let dealerDraw = makeCardName(getCard())
 
 //adjusting inner html to display the card for the player
-document.getElementById('playerHand').innerHTML = `<p>Player drew ${myDeck[getCard()]}</p>`;
+document.getElementById('playerHand').innerHTML = `<p>Player drew ${playerDraw}</p>`;
 
-document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${myDeck[getCard()]}</p>`;
+document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${dealerDraw}</p>`;
