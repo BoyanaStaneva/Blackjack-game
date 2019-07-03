7ea50e38c7dbeaddf510fcf73111d90c542ab5a2
Boyana Staneva <boyana@rudor.btn1.brandwatch.net>
2019-07-03 19:05:49 +0100

    Each card in the deck of cards now has a points property, which details the amount of points the card is worth

--- script.js
+++ script.js
@@ -13,29 +13,54 @@ function createDeck () {
       //pushing the combinations into a the deck array
       let card = {
         suit: suits[suitsIndx],
-        value: cards[cardsIndx]
+        value: cards[cardsIndx],
+        name: cards[cardsIndx] + ' of ' + suits[suitsIndx],
+        points: 5
       }
-      myDeck.push(card)
+      if (card.value == "Two") {
+        card.points = 2}
+      if (card.value == "Three") {
+          card.points = 3}
+      if (card.value == "Four") {
+          card.points = 4}
+      if (card.value == "Five") {
+          card.points = 5}
+      if (card.value == "Six") {
+          card.points = 6}
+      if (card.value == "Seven") {
+          card.points = 7}
+      if (card.value == "Eight") {
+          card.points = 8}
+      if (card.value == "Nine") {
+          card.points = 9 }
+      if (card.value == "Ten" || card.value == "Jack") {
+          card.points = 10}
+      if (card.value == "Queen" || card.value == "King") {
+          card.points = 10}
+      if (card.value == "Ace") {
+          card.points = 10}
+    myDeck.push(card)
+
     }
+
   }
   return myDeck
+
 }
+
+
 
 let makeDeck = createDeck();
-
-function makeCardName(i) {
-  return makeDeck[i].value + ' of ' + makeDeck[i].suit
-  console.log(makeDeck[i].value + ' of ' + makeDeck[i].suit);
-}
+console.log(makeDeck);
 
 function getCard() {
   return Math.floor((Math.random() * 51) + 1);
 }
 
-let playerDraw = makeCardName(getCard())
-let dealerDraw = makeCardName(getCard())
+let playerDraw = makeDeck[getCard()]
+let dealerDraw = makeDeck[getCard()]
 
 //adjusting inner html to display the card for the player
-document.getElementById('playerHand').innerHTML = `<p>Player drew ${playerDraw}</p>`;
+document.getElementById('playerHand').innerHTML = `<p>Player drew ${playerDraw.name}</p> <p>Player score is ${playerDraw.points}</p>`;
 
-document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${dealerDraw}</p>`;
+document.getElementById('dealerHand').innerHTML = `<p>Dealer drew ${dealerDraw.name}</p> <p>Player score is ${dealerDraw.points}</p>`;
